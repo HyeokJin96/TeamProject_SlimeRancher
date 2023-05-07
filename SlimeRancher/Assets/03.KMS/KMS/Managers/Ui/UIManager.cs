b821d6d7d6f859fc40e2bee950630f2b09252382
ms0000001 <alstn830@gmail.com>
2023-05-07 17:56:36 +0900

    [KMS] BuildTest & Add Door

--- SlimeRancher/Assets/03.KMS/KMS/Managers/Ui/UIManager.cs
+++ SlimeRancher/Assets/03.KMS/KMS/Managers/Ui/UIManager.cs
@@ -12,6 +12,9 @@ public class UIManager : KSingleton<UIManager>
     public GameObject buttons = default;
     public GameObject map = default;
 
+    public GameObject interactEP = default;
+    public GameObject interactE = default;
+
     //Play
     public GameObject PlayMenu = default;
 
@@ -152,6 +155,8 @@ public class UIManager : KSingleton<UIManager>
         //Caching
         UiObjs = GameObject.Find("UiObjs").gameObject;
         buttons = UiObjs.transform.GetChild(2).gameObject;
+        interactEP = UiObjs.transform.GetChild(0).gameObject;
+        interactE = interactEP.transform.GetChild(0).gameObject;
 
         #region Main Objects
         PlayMenu = UiObjs.transform.GetChild(5).gameObject;
@@ -703,6 +708,11 @@ public class UIManager : KSingleton<UIManager>
         {
             map.SetActive(false);
         }
+    }
+
+    public void InteractE()
+    {
+        
     }
     #endregion
 }
