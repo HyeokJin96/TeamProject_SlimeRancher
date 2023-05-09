688f7bb14a93f5de3b9c87dbd2431a4c9aa6b3c1
GX1018 <wjdghlxo@gmail.com>
2023-05-09 10:52:06 +0900

    [HT]Food vacuum modify

--- SlimeRancher/Assets/01.YHJ/02. Scripts/test_veggie.cs
+++ SlimeRancher/Assets/01.YHJ/02. Scripts/test_veggie.cs
@@ -31,8 +31,8 @@ public class test_veggie : MonoBehaviour
         rigidbody.isKinematic = true;
 
         initialScale = sprout_carrot.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-        startPosY = 28.29541f;
-        maxPosY = 28.4f;
+        //startPosY = 28.29541f;
+        //maxPosY = 28.4f;
 
     }
 
@@ -43,8 +43,8 @@ public class test_veggie : MonoBehaviour
             currentScale = currentScale + new Vector3(growthRate, growthRate, growthRate);
             sprout_carrot.transform.localScale = currentScale;
 
-            currentPosY = Mathf.Lerp(startPosY, maxPosY, currentScale.x);
-            transform.position = new Vector3(transform.position.x, currentPosY, transform.position.z);
+            //currentPosY = Mathf.Lerp(startPosY, maxPosY, currentScale.x);
+            //transform.position = new Vector3(transform.position.x, currentPosY, transform.position.z);
 
             if (currentScale.x >= 1f)
             {
--- SlimeRancher/Assets/02.HT/Scripts/Vac_Test.cs
+++ SlimeRancher/Assets/02.HT/Scripts/Vac_Test.cs
@@ -414,7 +414,7 @@ public class Vac_Test : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (fuckU && !isPickUpLargo)
+        if (fuckU && !isPickUpLargo &&other.GetType() == typeof(MeshCollider))
         {
             if (eeee.Contains(other.gameObject))
             {
@@ -439,14 +439,16 @@ public class Vac_Test : MonoBehaviour
                 }
 
                 // 대상 오브젝트 콜라이더 트리거 on
-                if(other.transform.parent.GetComponent<ObjecData>().foodName == FoodName.Carrot && other.transform.parent.GetComponent<test_veggie>().isGrowing == true)
+                /* if(other.transform.parent.GetComponent<ObjecData>().foodName == FoodName.Carrot && other.transform.parent.GetComponent<test_veggie>().isGrowing == true)
                 {
                     //donothing
                 }
                 else
                 {
-                other.GetComponent<MeshCollider>().isTrigger = true;
-
+                } */
+                if (other.GetType() == typeof(MeshCollider))
+                {
+                    other.GetComponent<MeshCollider>().isTrigger = true;
                 }
 
                 // 대상 오브젝트가 슬라임이면, state 변경
@@ -544,8 +546,8 @@ public class Vac_Test : MonoBehaviour
 
                 else if (other.tag == "Food" && vacuumedList.Contains(other.gameObject))
                 {
-                    if(other.transform.parent.GetComponent<ObjecData>().foodName == FoodName.Carrot && other.transform.parent.GetComponent<test_veggie>().isGrowing == false)
-                    {
+                    /* if(other.transform.parent.GetComponent<ObjecData>().foodName == FoodName.Carrot && other.transform.parent.GetComponent<test_veggie>().isGrowing == false)
+                    { */
 
                     dir = jointArray[nearestIndex].transform.position - other.transform.parent.position;
                     other.transform.parent.GetComponent<Rigidbody>().velocity = dir * 10f;
@@ -603,7 +605,7 @@ public class Vac_Test : MonoBehaviour
                             //nearestIndex = 0;
                         }
                     }
-                    }
+                    /* } */
                 }
 
 
